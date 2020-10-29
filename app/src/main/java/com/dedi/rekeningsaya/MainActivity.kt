dc83bd5e958cb5ca9377f59e15e436cb46bf5a88
dedidieu <62047093+dedidieu@users.noreply.github.com>
2020-10-29 23:16:12 +0700

    Initial commit

---
+++ app/src/main/java/com/dedi/rekeningsaya/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.dedi.rekeningsaya
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    private fun copyTextToClipboard() {
+        val textToCopy = tv_rek.text
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData = ClipData.newPlainText("text", textToCopy)
+        clipboardManager.setPrimaryClip(clipData)
+        Toast.makeText(this, "Tersalin!", Toast.LENGTH_LONG).show()
+
+    }
+
+    fun norek(view: View) {
+        copyTextToClipboard()
+    }
+    }
+
+
